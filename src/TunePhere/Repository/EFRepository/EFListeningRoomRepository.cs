0b5c36b40a2655a23f1ac76da7ae500a680c08b7
VanThinh512 <thinhtranlinux1@gmail.com>
2025-03-17 22:09:13 +0700

    Thinh_Change add EF & Intface Repository

---
+++ src/TunePhere/Repository/EFRepository/EFListeningRoomRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TunePhere.Models;
+using TunePhere.Repository.IMPRepository;
+
+namespace TunePhere.Repository.EFRepository
+{
+    public class EFListeningRoomRepository : IListeningRoomRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EFListeningRoomRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ListeningRoom>> GetAllAsync()
+        {
+            return await _context.ListeningRooms.ToListAsync();
+        }
+
+        public async Task<ListeningRoom> GetByIdAsync(int roomId)
+        {
+            return await _context.ListeningRooms.FindAsync(roomId);
+        }
+
+        public async Task AddAsync(ListeningRoom room)
+        {
+            _context.ListeningRooms.Add(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(ListeningRoom room)
+        {
+            _context.ListeningRooms.Update(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int roomId)
+        {
+            var room = await _context.ListeningRooms.FindAsync(roomId);
+            if (room != null)
+            {
+                _context.ListeningRooms.Remove(room);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ src/TunePhere/Repository/IMPRepository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using TunePhere.Models;
+
+namespace TunePhere.Repository.IMPRepository
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int userId);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(int userId);
+    }
+}
