f33a72fe4bc3c7c018e1641a63796a4fa4e4295d
Tân Phạm <tanpham2421@gmail.com>
2025-03-17 20:06:55 +0700

    add [entityframework] [users.cs]

---
+++ src/TunePhere/Models/Users.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TunePhere.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; } // Khóa chính
+
+        [Required, StringLength(50)]
+        public string Username { get; set; } // Tên đăng nhập
+
+        [Required, EmailAddress, StringLength(100)]
+        public string Email { get; set; } // Email
+
+        [Required]
+        public string PasswordHash { get; set; } // Mã hóa mật khẩu
+
+        [StringLength(100)]
+        public string FullName { get; set; } // Họ và tên đầy đủ
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Ngày tạo tài khoản
+
+        public DateTime? LastLogin { get; set; } // Lần đăng nhập gần nhất
+    }
+}
