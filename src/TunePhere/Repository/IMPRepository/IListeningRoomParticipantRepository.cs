41ac6b06fcafd5adca2375483e8480341e8d6dbe
LeThanhNhan <lnhan8132@gmail.com>
2025-03-18 13:50:06 +0700

    UPDATE ALL

--- src/TunePhere/Repository/IMPRepository/IListeningRoomParticipantRepository.cs
+++ src/TunePhere/Repository/IMPRepository/IListeningRoomParticipantRepository.cs
@@ -5,7 +5,7 @@ namespace TunePhere.Repository.IMPRepository
     public interface IListeningRoomParticipantRepository
     {
         Task<IEnumerable<ListeningRoomParticipant>> GetAllAsync();
-        Task<ListeningRoomParticipant> GetByIdsAsync(int roomId, int userId);
+        Task<ListeningRoomParticipant?> GetByIdsAsync(int roomId, int userId);
         Task AddAsync(ListeningRoomParticipant participant);
         Task UpdateAsync(ListeningRoomParticipant participant);
         Task DeleteAsync(int roomId, int userId);
