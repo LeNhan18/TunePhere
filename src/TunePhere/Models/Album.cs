b9e7c426f40d60974db2288d872a5ccee5d9bf4a
LeThanhNhan <lnhan8132@gmail.com>
2025-03-24 22:54:28 +0700

    Add Album and Artist

---
+++ src/TunePhere/Models/Album.cs
@@ -0,0 +1,23 @@
+﻿namespace TunePhere.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+
+        public string? AlbumName { get; set; }
+        public string? AlbumDescription { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? ReleaseDate { get; set; }
+
+        public int numberSongs { get; set; }
+
+        public TimeSpan Time { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+        public Album() {
+            Songs = new HashSet<Song>();
+        }
+    }
+}
