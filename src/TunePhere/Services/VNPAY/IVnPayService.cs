8c9f30ac7d0a9b84188b7c4d6e3d9dddf71ce9a7
LeThanhNhan <lnhan8132@gmail.com>
2025-06-25 22:14:25 +0700

    Interface VNpay

---
+++ src/TunePhere/Services/VNPAY/IVnPayService.cs
@@ -0,0 +1,10 @@
+﻿using TunePhere.Models.VNPAY;
+
+namespace TunePhere.Services.VNPAY
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
